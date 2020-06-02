f65f8564e526dfc3c4a979d9388617fc695f69df
Akhil Mohan <akhil.mohan@mayadata.io>
2020-06-02 23:37:36 +0530

    disk detection of mayastor
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- Go/mayastordisk.go
+++ Go/mayastordisk.go
@@ -2,9 +2,7 @@
 
 package main
 
-
 /*
- #cgo LDFLAGS: -lblkid
 #include "string.h"
 #include "stdlib.h"
 #include "stdint.h"
@@ -17,36 +15,34 @@ typedef struct {
         char bstype[SPDK_BLOBSTORE_TYPE_LENGTH];
 }spdk_bs_type;
 
-typedef struct  {
+struct spdk_bs_super_block {
         uint8_t         signature[8];
         uint32_t        version;
         uint32_t        length;
         uint32_t        clean;
 		spdk_blob_id    super_blob;
-
 		uint32_t        cluster_size;
-
 		uint32_t        used_page_mask_start;
 		uint32_t        used_page_mask_len;
-
 		uint32_t        used_cluster_mask_start;
 		uint32_t        used_cluster_mask_len;
-
 		uint32_t        md_start;
 		uint32_t        md_len;
-
 		spdk_bs_type     bstype;
-
 		uint32_t        used_blobid_mask_start;
 		uint32_t        used_blobid_mask_len;
-
 		uint64_t        size;
 		uint32_t        io_unit_size;
-
 		uint8_t         reserved[4000];
 		uint32_t        crc;
-}spdk_bs_super_block;
+};
+
+char *get_signature(struct spdk_bs_super_block *spdk)
+{
+	return spdk->signature;
+}
 */
+
 import "C"
 import (
 	"encoding/binary"
@@ -55,15 +51,13 @@ import (
 	"os"
 )
 
+const spdk_signature = "SPDKBLOB"
+
 
 func main()  {
 	dev := os.Args[1]
-
-	/*var spdkblob *C.char
-	spdkblob = C.CString("SPDKBLOB")*/
-
-	var spdk C.spdk_bs_super_block
-
+	var spdk *C.struct_spdk_bs_super_block
+	buf := make([]byte, C.sizeof_struct_spdk_bs_super_block)
 	f, err := os.Open(dev)
 	if err != nil {
 		fmt.Println("error opning", err)
@@ -74,18 +68,23 @@ func main()  {
 		fmt.Println("error seeking", err)
 		return
 	}
-
-	err = binary.Read(f, binary.BigEndian, &spdk)
+	err = binary.Read(f, binary.LittleEndian, buf)
 	if err != nil {
 		fmt.Println("error reading", err)
 		return
 	}
+	spdk = (*C.struct_spdk_bs_super_block)(C.CBytes(buf))
+	var ptr *C.char
+	ptr = (*C.char)(C.get_signature(spdk))
+	s := C.GoString(ptr)
 
-/*	var ptr *C.char
-	ptr = (*C.char)(spdk.signature[0])
-	s := C.GoString(ptr)*/
+	// take only the first 8 characters for signature
+	s = s[0:8]
 
-	fmt.Println(spdk.signature)
+	if s == spdk_signature {
+		fmt.Println("in use by mayastor")
+	} else {
+		fmt.Println("not in use by mayastor")
+	}
 
 }
-
